bc0f27c4a85a82cfc02b23ac8f8ad772f84ff57a
GitHub Action <action@github.com>
2020-12-19 18:30:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/wolf/mergeintomainplugin/MyBundle.kt
+++ src/main/kotlin/com/github/wolf/mergeintomainplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.wolf.mergeintomainplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/wolf/mergeintomainplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wolf/mergeintomainplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wolf.mergeintomainplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wolf.mergeintomainplugin.MyBundle
 
 class MyApplicationService {
 
